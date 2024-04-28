55def969ae90e9fe3a24ebd5bdec711832e9f344
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-04-28 13:15:54 -0400

    updated forge

--- src/main/java/net/marsh/spacecraft/block/entity/CoalGeneratorBlockEntity.java
+++ src/main/java/net/marsh/spacecraft/block/entity/CoalGeneratorBlockEntity.java
@@ -18,8 +18,8 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
@@ -77,7 +77,7 @@ public class CoalGeneratorBlockEntity extends BlockEntity implements MenuProvide
 
     @Override
     public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
-        if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+        if(cap == ForgeCapabilities.ITEM_HANDLER) {
             return lazyItemHandler.cast();
         }
 
--- src/main/java/net/marsh/spacecraft/screen/CoalGeneratorMenu.java
+++ src/main/java/net/marsh/spacecraft/screen/CoalGeneratorMenu.java
@@ -9,7 +9,7 @@ import net.minecraft.world.inventory.*;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.items.SlotItemHandler;
 
 public class CoalGeneratorMenu extends AbstractContainerMenu {
@@ -31,7 +31,7 @@ public class CoalGeneratorMenu extends AbstractContainerMenu {
         addPlayerInventory(inv);
         addPlayerHotbar(inv);
 
-        this.blockEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(handler -> {
+        this.blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(handler -> {
             this.addSlot(new SlotItemHandler(handler, 0, 80, 61));
             this.addSlot(new SlotItemHandler(handler, 1, 152, 61));
         });
