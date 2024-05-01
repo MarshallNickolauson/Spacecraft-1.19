85181afa0849fd3c6c368ac0e86fe90e70fc8745
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-05-01 12:36:53 -0400

    simplified oreCooking recipe data generation

--- src/main/java/net/marsh/spacecraft/datagen/ModRecipeProvider.java
+++ src/main/java/net/marsh/spacecraft/datagen/ModRecipeProvider.java
@@ -5,6 +5,8 @@ import net.marsh.spacecraft.item.ModItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
 import java.util.List;
@@ -18,26 +20,19 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
     @Override
     protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
-
-        //TODO simplify this into a function that does both at once
-
-        oreSmelting(consumer, List.of(ModItems.RAW_ALUMINUM.get(), ModBlocks.ALUMINUM_ORE.get(), ModBlocks.DEEPSLATE_ALUMINUM_ORE.get()), ModItems.ALUMINUM_INGOT.get(), 0.7F, 200, "aluminum");
-        oreSmelting(consumer, List.of(ModItems.RAW_LEAD.get(), ModBlocks.LEAD_ORE.get(), ModBlocks.DEEPSLATE_LEAD_ORE.get()), ModItems.LEAD_INGOT.get(), 0.7F, 200, "lead");
-        oreSmelting(consumer, List.of(ModItems.RAW_METEORIC_IRON.get(), ModBlocks.METEORIC_IRON_ORE.get(), ModBlocks.DEEPSLATE_METEORIC_IRON_ORE.get()), ModItems.METEORIC_IRON_INGOT.get(), 0.7F, 200, "meteoric_iron");
-        oreSmelting(consumer, List.of(ModItems.RAW_PALLADIUM.get(), ModBlocks.PALLADIUM_ORE.get(), ModBlocks.DEEPSLATE_PALLADIUM_ORE.get()), ModItems.PALLADIUM_INGOT.get(), 0.7F, 200, "palladium");
-        oreSmelting(consumer, List.of(ModBlocks.SILICON_ORE.get(), ModBlocks.DEEPSLATE_SILICON_ORE.get()), ModItems.RAW_SILICON.get(), 0.7F, 200, "silicon");
-        oreSmelting(consumer, List.of(ModItems.RAW_TIN.get(), ModBlocks.TIN_ORE.get(), ModBlocks.DEEPSLATE_TIN_ORE.get()), ModItems.TIN_INGOT.get(), 0.7F, 200, "tin");
-        oreSmelting(consumer, List.of(ModItems.RAW_TITANIUM.get(), ModBlocks.TITANIUM_ORE.get(), ModBlocks.DEEPSLATE_TITANIUM_ORE.get()), ModItems.TITANIUM_INGOT.get(), 0.7F, 200, "titanium");
-        oreSmelting(consumer, List.of(ModItems.RAW_URANIUM.get(), ModBlocks.URANIUM_ORE.get(), ModBlocks.DEEPSLATE_URANIUM_ORE.get()), ModItems.URANIUM_INGOT.get(), 0.7F, 200, "uranium");
-
-
-        oreBlasting(consumer, List.of(ModItems.RAW_ALUMINUM.get(), ModBlocks.ALUMINUM_ORE.get(), ModBlocks.DEEPSLATE_ALUMINUM_ORE.get()), ModItems.ALUMINUM_INGOT.get(), 0.7F, 200, "aluminum");
-        oreBlasting(consumer, List.of(ModItems.RAW_LEAD.get(), ModBlocks.LEAD_ORE.get(), ModBlocks.DEEPSLATE_LEAD_ORE.get()), ModItems.LEAD_INGOT.get(), 0.7F, 200, "lead");
-        oreBlasting(consumer, List.of(ModItems.RAW_METEORIC_IRON.get(), ModBlocks.METEORIC_IRON_ORE.get(), ModBlocks.DEEPSLATE_METEORIC_IRON_ORE.get()), ModItems.METEORIC_IRON_INGOT.get(), 0.7F, 200, "meteoric_iron");
-        oreBlasting(consumer, List.of(ModItems.RAW_PALLADIUM.get(), ModBlocks.PALLADIUM_ORE.get(), ModBlocks.DEEPSLATE_PALLADIUM_ORE.get()), ModItems.PALLADIUM_INGOT.get(), 0.7F, 200, "palladium");
-        oreBlasting(consumer, List.of(ModBlocks.SILICON_ORE.get(), ModBlocks.DEEPSLATE_SILICON_ORE.get()), ModItems.RAW_SILICON.get(), 0.7F, 200, "silicon");
-        oreBlasting(consumer, List.of(ModItems.RAW_TIN.get(), ModBlocks.TIN_ORE.get(), ModBlocks.DEEPSLATE_TIN_ORE.get()), ModItems.TIN_INGOT.get(), 0.7F, 200, "tin");
-        oreBlasting(consumer, List.of(ModItems.RAW_TITANIUM.get(), ModBlocks.TITANIUM_ORE.get(), ModBlocks.DEEPSLATE_TITANIUM_ORE.get()), ModItems.TITANIUM_INGOT.get(), 0.7F, 200, "titanium");
-        oreBlasting(consumer, List.of(ModItems.RAW_URANIUM.get(), ModBlocks.URANIUM_ORE.get(), ModBlocks.DEEPSLATE_URANIUM_ORE.get()), ModItems.URANIUM_INGOT.get(), 0.7F, 200, "uranium");
+        oreCooking(consumer, List.of(ModItems.RAW_ALUMINUM.get(), ModBlocks.ALUMINUM_ORE.get(), ModBlocks.DEEPSLATE_ALUMINUM_ORE.get()), ModItems.ALUMINUM_INGOT.get(), 0.7F, 200, "aluminum");
+        oreCooking(consumer, List.of(ModItems.RAW_LEAD.get(), ModBlocks.LEAD_ORE.get(), ModBlocks.DEEPSLATE_LEAD_ORE.get()), ModItems.LEAD_INGOT.get(), 0.7F, 200, "lead");
+        oreCooking(consumer, List.of(ModItems.RAW_METEORIC_IRON.get(), ModBlocks.METEORIC_IRON_ORE.get(), ModBlocks.DEEPSLATE_METEORIC_IRON_ORE.get()), ModItems.METEORIC_IRON_INGOT.get(), 0.7F, 200, "meteoric_iron");
+        oreCooking(consumer, List.of(ModItems.RAW_PALLADIUM.get(), ModBlocks.PALLADIUM_ORE.get(), ModBlocks.DEEPSLATE_PALLADIUM_ORE.get()), ModItems.PALLADIUM_INGOT.get(), 0.7F, 200, "palladium");
+        oreCooking(consumer, List.of(ModBlocks.SILICON_ORE.get(), ModBlocks.DEEPSLATE_SILICON_ORE.get()), ModItems.RAW_SILICON.get(), 0.7F, 200, "silicon");
+        oreCooking(consumer, List.of(ModItems.RAW_TIN.get(), ModBlocks.TIN_ORE.get(), ModBlocks.DEEPSLATE_TIN_ORE.get()), ModItems.TIN_INGOT.get(), 0.7F, 200, "tin");
+        oreCooking(consumer, List.of(ModItems.RAW_TITANIUM.get(), ModBlocks.TITANIUM_ORE.get(), ModBlocks.DEEPSLATE_TITANIUM_ORE.get()), ModItems.TITANIUM_INGOT.get(), 0.7F, 200, "titanium");
+        oreCooking(consumer, List.of(ModItems.RAW_URANIUM.get(), ModBlocks.URANIUM_ORE.get(), ModBlocks.DEEPSLATE_URANIUM_ORE.get()), ModItems.URANIUM_INGOT.get(), 0.7F, 200, "uranium");
     }
+
+    private static void oreCooking(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
+        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.SMELTING_RECIPE, pIngredients, pResult, pExperience, pCookingTime, pGroup, "_from_smelting");
+        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.BLASTING_RECIPE, pIngredients, pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
+    }
+
 }
