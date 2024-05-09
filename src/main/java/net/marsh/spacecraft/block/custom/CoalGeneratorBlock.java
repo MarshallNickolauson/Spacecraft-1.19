71a6eec4de29371a725d792aaae829eb65156bb1
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-05-09 07:34:42 -0400

    added loot tables for machines

--- src/main/java/net/marsh/spacecraft/block/custom/CoalGeneratorBlock.java
+++ src/main/java/net/marsh/spacecraft/block/custom/CoalGeneratorBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
