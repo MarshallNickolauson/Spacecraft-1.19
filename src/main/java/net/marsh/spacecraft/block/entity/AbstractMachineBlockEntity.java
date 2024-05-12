a7983c479834fe6a55cdf6ca9ffb01a0a546abed
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-05-12 14:22:33 -0400

    began block entity abstraction for machines

---
+++ src/main/java/net/marsh/spacecraft/block/entity/AbstractMachineBlockEntity.java
@@ -0,0 +1,93 @@
+package net.marsh.spacecraft.block.entity;
+
+import net.marsh.spacecraft.util.ModBlockEnergyStorage;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.Containers;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.energy.IEnergyStorage;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class AbstractMachineBlockEntity extends BlockEntity implements MenuProvider {
+    protected ModBlockEnergyStorage ENERGY_STORAGE;
+    protected final ItemStackHandler itemHandler;
+    protected final ContainerData data;
+    protected LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
+    protected LazyOptional<IEnergyStorage> lazyEnergyHandler = LazyOptional.empty();
+
+    public AbstractMachineBlockEntity(BlockEntityType<?> pType, BlockPos pPos, BlockState pBlockState) {
+        super(pType, pPos, pBlockState);
+        this.itemHandler = createItemHandler();
+        this.ENERGY_STORAGE = createEnergyStorage();
+        this.data = createContainerData();
+    }
+
+    protected abstract ContainerData createContainerData();
+
+    protected abstract ItemStackHandler createItemHandler();
+
+    protected abstract ModBlockEnergyStorage createEnergyStorage();
+
+    @Override
+    public abstract @NotNull Component getDisplayName();
+
+    @Nullable
+    @Override
+    public abstract AbstractContainerMenu createMenu(int i, Inventory inventory, Player player);
+
+    public IEnergyStorage getEnergyStorage() { return ENERGY_STORAGE; }
+
+    public void setEnergyLevel(int energy) { this.ENERGY_STORAGE.setEnergy(energy); }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        lazyItemHandler = LazyOptional.of(() -> itemHandler);
+        lazyEnergyHandler = LazyOptional.of(() -> ENERGY_STORAGE);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        lazyItemHandler.invalidate();
+        lazyEnergyHandler.invalidate();
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag nbt) {
+        nbt.put("inventory", itemHandler.serializeNBT());
+        nbt.putInt("energy", ENERGY_STORAGE.getEnergyStored());
+        super.saveAdditional(nbt);
+    }
+
+    @Override
+    public void load(@NotNull CompoundTag nbt) {
+        super.load(nbt);
+        itemHandler.deserializeNBT(nbt.getCompound("inventory"));
+        ENERGY_STORAGE.setEnergy(nbt.getInt("energy"));
+    }
+
+    public void drops() {
+        SimpleContainer inventory = new SimpleContainer(itemHandler.getSlots());
+        for (int i = 0; i < itemHandler.getSlots(); i++) {
+            inventory.setItem(i, itemHandler.getStackInSlot(i));
+        }
+
+        assert this.level != null;
+        Containers.dropContents(this.level, this.worldPosition, inventory);
+    }
+}
--- src/main/java/net/marsh/spacecraft/block/entity/SteelFoundryBlockEntity.java
+++ src/main/java/net/marsh/spacecraft/block/entity/SteelFoundryBlockEntity.java
@@ -1,21 +1,16 @@
 package net.marsh.spacecraft.block.entity;
 
 import net.marsh.spacecraft.block.ModBlockEntities;
-import net.marsh.spacecraft.block.WrappedHandler;
 import net.marsh.spacecraft.block.custom.SteelFoundryBlock;
 import net.marsh.spacecraft.item.ModItems;
 import net.marsh.spacecraft.networking.ModMessages;
 import net.marsh.spacecraft.networking.packet.SteelFoundryEnergySyncS2CPacket;
 import net.marsh.spacecraft.render.menu.SteelFoundryMenu;
-import net.marsh.spacecraft.sound.ModSounds;
 import net.marsh.spacecraft.util.ModBlockEnergyStorage;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.Containers;
-import net.minecraft.world.MenuProvider;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -24,71 +19,45 @@ import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.energy.IEnergyStorage;
-import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Map;
+public class SteelFoundryBlockEntity extends AbstractMachineBlockEntity {
 
-@SuppressWarnings("ALL")
-public class SteelFoundryBlockEntity extends BlockEntity implements MenuProvider {
+//
+//    private final Map<Direction, LazyOptional<WrappedHandler>> directionWrappedHandlerMap =
+//            Map.of(
+//                    Direction.UP, LazyOptional.of(() -> new WrappedHandler(itemStackHandler, (index) -> index == 1, (index, stack) -> itemStackHandler.isItemValid(1, stack))),
+//                    Direction.DOWN, LazyOptional.of(() -> new WrappedHandler(itemStackHandler, (i) -> i == 2, (i, s) -> false)),
+//                    Direction.NORTH, LazyOptional.of(() -> new WrappedHandler(itemStackHandler, (index) -> index == 1, (index, stack) -> itemStackHandler.isItemValid(1, stack))),
+//                    Direction.SOUTH, LazyOptional.of(() -> new WrappedHandler(itemStackHandler, (i) -> i == 2, (i, s) -> false)),
+//                    Direction.WEST, LazyOptional.of(() -> new WrappedHandler(itemStackHandler, (i) -> i == 2, (i, s) -> false)),
+//                    Direction.EAST, LazyOptional.of(() -> new WrappedHandler(itemStackHandler, (index) -> index == 1, (index, stack) -> itemStackHandler.isItemValid(1, stack)))
+//            );
 
-    private final ItemStackHandler itemHandler = new ItemStackHandler(4) {
-        @Override
-        protected void onContentsChanged(int slot) {
-            setChanged();
-        }
-
-        @Override
-        public boolean isItemValid(int slot, @NotNull ItemStack stack) {
-            return switch (slot) {
-                //TODO change case 1 to a battery type later on. Make abstract class
-                case 0 -> stack.getItem() == Items.DIAMOND;
-                case 1 -> stack.getItem() == Items.IRON_INGOT;
-                case 2 -> stack.getItem() == ModItems.CARBON_FRAGMENTS.get();
-                case 3 -> false;
-                default -> super.isItemValid(slot, stack);
-            };
-        }
-    };
-
-    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
-    private final Map<Direction, LazyOptional<WrappedHandler>> directionWrappedHandlerMap =
-            Map.of(
-                    Direction.UP, LazyOptional.of(() -> new WrappedHandler(itemHandler, (index) -> index == 1, (index, stack) -> itemHandler.isItemValid(1, stack))),
-                    Direction.DOWN, LazyOptional.of(() -> new WrappedHandler(itemHandler, (i) -> i == 2, (i, s) -> false)),
-                    Direction.NORTH, LazyOptional.of(() -> new WrappedHandler(itemHandler, (index) -> index == 1, (index, stack) -> itemHandler.isItemValid(1, stack))),
-                    Direction.SOUTH, LazyOptional.of(() -> new WrappedHandler(itemHandler, (i) -> i == 2, (i, s) -> false)),
-                    Direction.WEST, LazyOptional.of(() -> new WrappedHandler(itemHandler, (i) -> i == 2, (i, s) -> false)),
-                    Direction.EAST, LazyOptional.of(() -> new WrappedHandler(itemHandler, (index) -> index == 1, (index, stack) -> itemHandler.isItemValid(1, stack)))
-            );
-
-    private LazyOptional<IEnergyStorage> lazyEnergyHandler = LazyOptional.empty();
-
-    private final ModBlockEnergyStorage ENERGY_STORAGE;
     private static final int ENERGY_REQUIRED = 51;
     private final Direction facing;
     private final Direction energyInputDirection;
-    protected final ContainerData data;
     private int craftingProgress = 0;
     private int maxCraftingProgress = 100;
     private int diodeChargeProgress = 0;
     private int maxDiodeCharge = 100;
-    private boolean isSoundOn = false;
 
     public SteelFoundryBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.STEEL_FOUNDRY.get(), pos, state);
         this.facing = state.getValue(SteelFoundryBlock.FACING);
         this.energyInputDirection = state.getValue(SteelFoundryBlock.ENERGY_INPUT_DIRECTION);
+    }
 
-        this.data = new ContainerData() {
+
+    @Override
+    protected ContainerData createContainerData() {
+        return new ContainerData() {
             @Override
             public int get(int index) {
                 return switch (index) {
@@ -115,42 +84,42 @@ public class SteelFoundryBlockEntity extends BlockEntity implements MenuProvider
                 return 4;
             }
         };
+    }
 
-        this.ENERGY_STORAGE = new ModBlockEnergyStorage(3000, 250) {
+    @Override
+    protected ItemStackHandler createItemHandler() {
+        return new ItemStackHandler(4) {
+            @Override
+            protected void onContentsChanged(int slot) {
+                setChanged();
+            }
 
+            @Override
+            public boolean isItemValid(int slot, @NotNull ItemStack stack) {
+                return switch (slot) {
+                    case 0 -> stack.getItem() == Items.DIAMOND;
+                    case 1 -> stack.getItem() == Items.IRON_INGOT;
+                    case 2 -> stack.getItem() == ModItems.CARBON_FRAGMENTS.get();
+                    case 3 -> false;
+                    default -> super.isItemValid(slot, stack);
+                };
+            }
+        };
+    }
+
+    @Override
+    protected ModBlockEnergyStorage createEnergyStorage() {
+        return new ModBlockEnergyStorage(3000, 250) {
             @Override
             public void onEnergyChanged() {
                 setChanged();
                 ModMessages.sendToClients(new SteelFoundryEnergySyncS2CPacket(this.energy, getBlockPos()));
             }
-
-            @Override
-            public int receiveEnergy(int maxReceive, boolean simulate) {
-                if (SteelFoundryBlockEntity.this.facing != SteelFoundryBlockEntity.this.energyInputDirection) {
-                    return 0;
-                }
-
-                return super.receiveEnergy(maxExtract, simulate);
-            }
-
-            @Override
-            public int extractEnergy(int maxExtract, boolean simulate) {
-                if (!hasRecipeInProgress()) {
-                    return 0;
-                }
-
-                return super.extractEnergy(maxExtract, simulate);
-            }
-
-            private boolean hasRecipeInProgress() {
-                return hasRecipe(SteelFoundryBlockEntity.this) && craftingProgress < maxCraftingProgress;
-            }
-
         };
     }
 
     @Override
-    public Component getDisplayName() {
+    public @NotNull Component getDisplayName() {
         return Component.literal("Steel Foundry");
     }
 
@@ -158,14 +127,6 @@ public class SteelFoundryBlockEntity extends BlockEntity implements MenuProvider
     @Override
     public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
         return new SteelFoundryMenu(id, inventory, this, this.data);
-    }
-
-    public IEnergyStorage getEnergyStorage() {
-        return ENERGY_STORAGE;
-    }
-
-    public void setEnergyLevel(int energy) {
-        this.ENERGY_STORAGE.setEnergy(energy);
     }
 
     @Override
@@ -188,69 +149,43 @@ public class SteelFoundryBlockEntity extends BlockEntity implements MenuProvider
         }
 
         if (cap == ForgeCapabilities.ITEM_HANDLER) {
-            if (side == null) {
-                return lazyItemHandler.cast();
-            }
+            return lazyItemHandler.cast();
 
-            if (directionWrappedHandlerMap.containsKey(side)) {
-                Direction localDir = this.getBlockState().getValue(SteelFoundryBlock.FACING);
-
-                if (side == Direction.UP || side == Direction.DOWN) {
-                    return directionWrappedHandlerMap.get(side).cast();
-                }
-
-                return switch (localDir) {
-                    default -> directionWrappedHandlerMap.get(side.getOpposite()).cast();
-                    case EAST -> directionWrappedHandlerMap.get(side.getClockWise()).cast();
-                    case SOUTH -> directionWrappedHandlerMap.get(side).cast();
-                    case WEST -> directionWrappedHandlerMap.get(side.getCounterClockWise()).cast();
-                };
-            }
+//            if (side == null) {
+//                return lazyItemHandler.cast();
+//            }
+//
+//            if (directionWrappedHandlerMap.containsKey(side)) {
+//                Direction localDir = this.getBlockState().getValue(SteelFoundryBlock.FACING);
+//
+//                if (side == Direction.UP || side == Direction.DOWN) {
+//                    return directionWrappedHandlerMap.get(side).cast();
+//                }
+//
+//                return switch (localDir) {
+//                    default -> directionWrappedHandlerMap.get(side.getOpposite()).cast();
+//                    case EAST -> directionWrappedHandlerMap.get(side.getClockWise()).cast();
+//                    case SOUTH -> directionWrappedHandlerMap.get(side).cast();
+//                    case WEST -> directionWrappedHandlerMap.get(side.getCounterClockWise()).cast();
+//                };
+//            }
         }
 
         return super.getCapability(cap, side);
     }
 
     @Override
-    public void onLoad() {
-        super.onLoad();
-        lazyItemHandler = LazyOptional.of(() -> itemHandler);
-        lazyEnergyHandler = LazyOptional.of(() -> ENERGY_STORAGE);
-    }
-
-    @Override
-    public void invalidateCaps() {
-        super.invalidateCaps();
-        lazyItemHandler.invalidate();
-        lazyEnergyHandler.invalidate();
-    }
-
-    @Override
     protected void saveAdditional(CompoundTag nbt) {
-        nbt.put("inventory", itemHandler.serializeNBT());
         nbt.putInt("steel_foundry_crafting_progress", this.craftingProgress);
         nbt.putInt("steel_foundry_diode_progress", this.diodeChargeProgress);
-        nbt.putInt("steel_foundry.energy", ENERGY_STORAGE.getEnergyStored());
-
         super.saveAdditional(nbt);
     }
 
     @Override
-    public void load(CompoundTag nbt) {
+    public void load(@NotNull CompoundTag nbt) {
         super.load(nbt);
-        itemHandler.deserializeNBT(nbt.getCompound("inventory"));
         craftingProgress = nbt.getInt("steel_foundry_crafting_progress");
         diodeChargeProgress = nbt.getInt("steel_foundry_diode_progress");
-        ENERGY_STORAGE.setEnergy(nbt.getInt("steel_foundry.energy"));
-    }
-
-    public void drops() {
-        SimpleContainer inventory = new SimpleContainer(itemHandler.getSlots());
-        for (int i = 0; i < itemHandler.getSlots(); i++) {
-            inventory.setItem(i, itemHandler.getStackInSlot(i));
-        }
-
-        Containers.dropContents(this.level, this.worldPosition, inventory);
     }
 
     public static void tick(Level level, BlockPos pos, BlockState state, SteelFoundryBlockEntity entity) {
@@ -270,14 +205,10 @@ public class SteelFoundryBlockEntity extends BlockEntity implements MenuProvider
 
         if(entity.diodeChargeProgress > 0) {
             entity.ENERGY_STORAGE.extractEnergy(ENERGY_REQUIRED, false);
+            level.setBlockAndUpdate(pos, state.setValue(SteelFoundryBlock.LIT, true));
             setChanged(level, pos, state);
-            if(!entity.isSoundOn) {
-                level.setBlockAndUpdate(pos, state.setValue(SteelFoundryBlock.LIT, true));
-                entity.isSoundOn = true;
-            }
         } else {
             level.setBlockAndUpdate(pos, state.setValue(SteelFoundryBlock.LIT, false));
-            entity.isSoundOn = false;
             setChanged(level, pos, state);
         }
 
@@ -338,6 +269,7 @@ public class SteelFoundryBlockEntity extends BlockEntity implements MenuProvider
         return (
                 (inventory.getItem(1).getItem() == Items.IRON_INGOT)
                 && (inventory.getItem(2).getItem() == ModItems.CARBON_FRAGMENTS.get())
+                && canInsertItemIntoOutputSlot(inventory, ModItems.STEEL_INGOT.get().getDefaultInstance())
         );
     }
 
